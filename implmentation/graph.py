1e99affc453f0c63bdc9082c925ce331035ca1f5
Dawood Zargar <dawoodzargar08@gmail.com>
2024-09-10 10:09:30 +0530

    some practice in graphs

--- implmentation/graph.py
+++ implmentation/graph.py
@@ -2,75 +2,79 @@ from collections import deque
 
 class Graph:
     def __init__(self, n, edges, isDirected=True) -> None:
-
         # Create adjacency list
-        self.adjList = {i: [] for i in range(n)}  # Initialize an empty list for each node
+        self.adjList = {i: [] for i in range(n)}
         for ele in edges:
-            self.adjList[ele[0]].append(ele[1])  # Add the edge to the adjacency list
+            self.adjList[ele[0]].append(ele[1])
             if not isDirected:
-                self.adjList[ele[1]].append(ele[0])  # Add the reverse edge if undirected
+                self.adjList[ele[1]].append(ele[0])
 
         # Create adjacency matrix
-        self.adjMat = [[-1 for _ in range(n)] for _ in range(n)]  # Initialize matrix with -1
+        self.adjMat = [[-1 for _ in range(n)] for _ in range(n)]
         for ele in edges:
-            self.adjMat[ele[0]][ele[1]] = 1  # Mark the edge in the matrix
+            self.adjMat[ele[0]][ele[1]] = 1
             if not isDirected:
-                self.adjMat[ele[1]][ele[0]] = 1  # Mark the reverse edge if undirected
+                self.adjMat[ele[1]][ele[0]] = 1
 
+    def BFSR(self, val=0):
+        visited = {key: False for key in self.adjList}
+        ans = ""
 
-
-    def BFSR(self,val=0):
-        visited = {key:False for key in self.adjList}
-        queue= deque()
-        queue.append(val)
-
-        ans =""
-        def _BFS():
+        def _BFS(queue):
             nonlocal ans
-            n =len(queue)
-            if n != 0:
-                for _ in range(n):
-                    front_value = queue.popleft()
-                    if visited[front_value]:
-                        continue
-                    else:
-                        ans = ans + str(front_value) +" "
-                        for ele in self.adjList[front_value]:
-                            queue.append(ele)
-                        visited[front_value] = True
+            if not queue:
+                return
 
+            n = len(queue)
+            for _ in range(n):
+                front_value = queue.popleft()
+                if visited[front_value]:
+                    continue
+                ans += str(front_value) + " "
+                visited[front_value] = True
+                for ele in self.adjList[front_value]:
+                    if not visited[ele]:
+                        queue.append(ele)
 
+            _BFS(queue)
 
-                _BFS()
+        queue = deque([val])
+        _BFS(queue)
+        return ans.strip()
 
-            return
-
-
-        _BFS()
-        return ans
-
-
-    def BFSI(self,val =0):
+    def BFSI(self, val=0):
         visited = [False for _ in self.adjList]
-        queue = deque()
-        queue.append(val)
+        queue = deque([val])
         ans = ""
 
         while queue:
             n = len(queue)
-
-            for i in range(n):
+            for _ in range(n):
                 front_value = queue.popleft()
                 if not visited[front_value]:
-                    ans =ans + str(front_value) + " "
+                    ans += str(front_value) + " "
                     visited[front_value] = True
-
                     for ele in self.adjList[front_value]:
-                        queue.append(ele)
-            ans = ans + ","
+                        if not visited[ele]:
+                            queue.append(ele)
+            ans = ans.strip() + ","
 
+        return ans.strip(',')
 
-        return ans
+    def DFS(self, start):
+        visited = {key: False for key in self.adjList}
+        ans = ""
+
+        def _DFS(val):
+            if visited[val]:return
+            nonlocal ans
+            ans += str(val) + " "
+            visited[val] = True
+            for ele in self.adjList[val]:
+                    _DFS(ele)
+
+        _DFS(start)
+        return ans.strip()
 
     def printAdjList(self):
         """Prints the adjacency list of the graph."""
@@ -82,12 +86,12 @@ class Graph:
         for row in self.adjMat:
             print(row)
 
-
 # Test cases to demonstrate the functionality of the Graph class
 print("--------Test Graph 1--------")
-edges = [(0, 1), (0, 2), (1, 2), (2, 3),(0,4),(1,5),(1,6),(6,7),(6,8),(2,9)]
+edges = [(0, 1), (0, 2), (1, 2), (2, 3), (0, 4), (1, 5), (1, 6), (6, 7), (6, 8), (2, 9)]
 g1 = Graph(10, edges, False)  # Creating an undirected graph
 print("Undirected Graph - Adjacency List:")
 g1.printAdjList()  # Should print adjacency list for undirected graph
-print(g1.BFSR())
-print(g1.BFSI())
+print("BFS Recursive:", g1.BFSR())
+print("BFS Iterative:", g1.BFSI())
+print("DFS:", g1.DFS(0))
